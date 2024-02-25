0e5bfc79a12a3763e2b08da6dc3be05a6e81a827
Chrystian Ariel <chrystian.ariel@gmail.com>
2024-02-25 14:09:55 -0300

    Initial setup and accordion component

---
+++ src/components/accordion/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id : '1',
+        question : "What are memes?",
+        answer : 
+        "Memes are an important part of the lifecycle in the internet."
+    },
+    {
+        id : '2',
+        question : "Why use memes?",
+        answer : 
+        "Young people only use this to express themselves, also because makes you look 'cool' for them."
+    },
+    {
+        id : '3',
+        question : "How to start using memes?",
+        answer : 
+        "If you don't know do not worry you'll get along with the images."
+    },
+    {
+        id : '4',
+        question : "Should I go to the doctor?",
+        answer : 
+        "Yes, you probably already need one right now. Just ask for the regular stuff and you'll be alright"
+    }
+]
+
+export default data;
